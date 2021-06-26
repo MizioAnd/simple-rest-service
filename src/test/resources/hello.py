6a79b2d3dae184f0a4c49efd18520fb74c212ee7
Andersen <mizio.spatafora@gmail.com>
2021-06-26 17:30:34 +0200

    Confirms that python exception is shown as part of json in microservice output

--- src/test/resources/hello.py
+++ src/test/resources/hello.py
@@ -1,3 +1,7 @@
 print("Hello Python here")
-# Test that pyexception is thrown
+
+# Test that pyexception is thrown.
+# Microservice output shows python exception in json when
+# $ curl http://localhost:8080/greeting | jq '.'
+#  /src/test/resources/hello.py\", line 3, in <module>,     import syds, ImportError: No module named syds
 import syds
