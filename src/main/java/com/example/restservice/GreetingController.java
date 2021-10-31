44e0a830383b3b2335af72abeea0359e8b4e9c68
Andersen <mizio.spatafora@gmail.com>
2021-10-31 14:55:30 +0100

    Set up api for R output

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -2,6 +2,7 @@ package com.example.restservice;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,9 @@ public class GreetingController {
 
     private static final String template = "Hello, %s";
     private final AtomicLong counter = new AtomicLong();
+
+    @Value("${server.port}")
+    String serverPort;
 
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) throws Exception {
@@ -26,8 +30,8 @@ public class GreetingController {
 
         // return new Greeting(counter.incrementAndGet(), String.format(template, "hello"));
         // return new Greeting(counter.incrementAndGet(), String.format(template, name));
-        return new Greeting(counter.incrementAndGet(), String.format(template, out));
-        // return new Greeting(counter.incrementAndGet(), String.format(template, mean));
+//        return new Greeting(counter.incrementAndGet(), String.format(template, out));
+         return new Greeting(counter.incrementAndGet(), String.format(template, mean));
     }
 
     @GetMapping("/greeting/version")
