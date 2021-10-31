f37ca87a8fef0e8198f9c7e9cf38e60b8f908fb0
Andersen <mizio.spatafora@gmail.com>
2021-10-31 15:07:24 +0100

    Set up api for python

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -2,6 +2,7 @@ package com.example.restservice;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,9 @@ public class GreetingController {
 
     private static final String template = "Hello, %s";
     private final AtomicLong counter = new AtomicLong();
+
+    @Value("${server.port}")
+    String serverPort;
 
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) throws Exception {
@@ -32,6 +36,6 @@ public class GreetingController {
 
     @GetMapping("/greeting/version")
     public String getVersionInfo(){
-        return "Version - V1";
+        return "Version - V2";
     }
 }
